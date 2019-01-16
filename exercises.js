b9202e03849660e4e305f1e6d8868c13777d3455
Kevin Guo <Kevin@Kevins-Air.home>
2019-01-15 23:11:53 -1000

    #3a

--- exercises.js
+++ exercises.js
@@ -47,13 +47,31 @@ This function will iterate through the person parameter and console.log the foll
 "President person was a great leader."
 */
 
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+
+for(var i = 0; i<presidents.length; i++){
+   console.log('The value at ' + i + ' is ' + presidents[i] + '.')
+}
+
+//3-A
+function leaders(person){
+ for(var i = 0; i < person.length; i++){   
+   console.log('President ' + person[i] + ' was a great leader.')
+   }
+}
+leaders(presidents);
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
 
+var stringOfNumbers = '';
 
+for(i = 10; i<21; i++){
+   stringOfNumbers.toString([i])
+}
+console.log(stringOfNumbers);
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
 
